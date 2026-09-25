0db52c8fc5278c96d4239358c50d964b32c6cb3b
Aman Jain <89681628+AmanJainA@users.noreply.github.com>
2026-09-25 20:14:46 +0530

    Connect src/data/writings.js to Supabase

--- src/data/writings.js
+++ src/data/writings.js
@@ -1,14 +1,2 @@
-const allWritings = [
-  {
-    id: 'blog-post-1',
-    title: 'My First Blog Post',
-    url: 'https://example.com/my-first-blog-post',
-  },
-  {
-    id: 'blog-post-2',
-    title: 'Another Interesting Read',
-    url: 'https://example.com/another-interesting-read',
-  },
-];
-
-export { allWritings };
+import { getWritings } from '../lib/supabase';
+export { getWritings };
