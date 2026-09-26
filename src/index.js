8224ff24c795e7a2a1d2d0e15ee5bcf26d34e6f7
Aman Jain <89681628+AmanJainA@users.noreply.github.com>
2026-09-26 15:03:20 +0530

    Run portfolio at root path without /anishP

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import AppWrapper from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/anishP">
+    <BrowserRouter>
       <AppWrapper />
     </BrowserRouter>
   </React.StrictMode>
